14d13a3a597441ee869003cf09cb555fce0a53e6
Eva Simon <eva.simon@gmail.com>
2017-10-25 13:00:25 -0500

    adds post method to reserve.html

--- server.js
+++ server.js
@@ -11,7 +11,22 @@ var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());	
+app.use(bodyParser.json());
+
+var path = require('path');
+
+// module.exports = function (app) {
+    
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname, "./public/home.html"));
+    })
+    app.get('/tables', function(req, res){
+        res.sendFile(path.join(__dirname, "./public/tables.html"));
+    })
+    app.get('/reserve', function(req, res){
+        res.sendFile(path.join(__dirname, "./public/reserve.html"));
+    })
+// }
 
 
 // Starts the server to begin listening
